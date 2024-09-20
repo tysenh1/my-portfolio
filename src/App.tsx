263458e143783d734e8fea635328191dbca93c19
Tysen Heck <theck@academic.rrc.ca>
2024-09-20 16:48:25 -0500

    added a guitar section and a function to handle button presses in the navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,40 @@ import viteLogo from '/vite.svg'
 import './styles.css'
 import Coding from './components/Coding'
 
+
+function handleNavClick(id: string) {
+  if (id == 'Coding') {
+    console.log('Coding yo')
+    document.getElementById('main-content-container').innerHTML = <Coding />
+  } else if (id == 'Guitar') {
+    console.log('Gutiar yo')
+  } else if (id == 'Other') {
+    console.log('A secret third thing yo')
+  } else {
+    console.log('no idea how you got here fam')
+  }
+}
+
 function App() {
 
 
   return (
     <body>
       <header className='header'>
-        <h1 className='website-title'>Welcome to my Portfolio!</h1>
+        <h1 className='website-title'>My Portfolio</h1>
         <nav className='navbar'>
           <ul className='navbar-list'>
-            <li className='navbar-item'><a className='navbar-link'>Thing 1</a></li>
-            <li className='navbar-item'><a className='navbar-link'>Thing 2</a></li>
-            <li className='navbar-item'><a className='navbar-link'>Thing 3</a></li>
+            <li className='navbar-item'><button onClick={(() => handleNavClick('Coding'))}>Thing 1</button></li>
+            <li className='navbar-item'><button onClick={(() => handleNavClick('Guitar'))}>Thing 2</button></li>
+            <li className='navbar-item'><button onClick={(() => handleNavClick('Other'))}>Thing 3</button></li>
           </ul>
         </nav>
       </header>
 
       <main>
-        <Coding />
+        <div id='main-content-container'>
+
+        </div>
       </main>
     </body>
   )
---
+++ src/components/Guitar.tsx
@@ -0,0 +1,13 @@
+
+
+function Guitar() {
+
+
+    return (
+        <>
+            <h2 className="article-header">Guitar</h2>
+        </>
+    )
+}
+
+export default Guitar
