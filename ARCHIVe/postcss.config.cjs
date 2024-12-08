0aef7974c885cb766e2eca44294ae8b36eb53aee
Tysen Heck <theck@academic.rrc.ca>
2024-12-07 23:32:34 -0600

    Created a new react project and archived the old one

---
+++ ARCHIVe/postcss.config.cjs
@@ -0,0 +1,7 @@
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+    plugins: [tailwindcss('./tailwind.config.cjs'), autoprefixer],
+};
+
