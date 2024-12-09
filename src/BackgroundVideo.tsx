27ccdae8e98a8673ac418ed798408684b862ec38
Tysen Heck <theck@academic.rrc.ca>
2024-12-09 13:38:01 -0600

    Settled on the video and put it into a new component and added some text to the home screen.

---
+++ src/BackgroundVideo.tsx
@@ -0,0 +1,15 @@
+
+
+function BackgroundVideo() {
+    return (
+        <div className={"opacity-50 absolute -z-50 top-0"}>
+            <video autoPlay loop muted
+                   className="object-cover mask-repeat-no-repeat mask-size-cover mask-image-radial-at-c from-black to-transparent to-50% blur-2xl h-[954px]">
+                <source src="https://raw.githubusercontent.com/tysenh1/my-portfolio/refs/heads/main/src/assets/chill-blue-blob-slow-SHORT.mp4" type="video/mp4"/>
+            </video>
+        </div>
+    
+    )
+}
+
+export default BackgroundVideo
---
+++ src/components/Home.tsx
@@ -0,0 +1,20 @@
+
+
+function Home() {
+    
+    
+    return (
+        <>
+            <div className={"grid justify-center content-center text-center font-roboto font-medium w-[1920px] h-[954px] text-5xl text-white"}>
+                <h1 className={""}>Hi, I'm Tysen.</h1>
+                <h1 className={""}>I make websites</h1>
+            </div>
+            <div className={"absolute top-[800px]"}>
+                <p></p>
+                <img src={"./src/assets/arrow-left.svg"}/>
+            </div>
+        </>
+    )
+}
+
+export default Home;
