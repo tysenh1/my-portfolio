36a17ec8cbf37189ffb15f767114a9ab06dd128b
Tysen Heck <theck@academic.rrc.ca>
2024-12-09 13:38:01 -0600

    Settled on the video and put it into a new component and added some text to the home screen.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
 import "./index.css";
 import Header from "./components/Header.tsx";
+import BackgroundVideo from "./BackgroundVideo.tsx";
+import Home from "./components/Home.tsx";
 
 
 function App() {
     
     return (
         <>
-            <Header/>
-            {/*<div className="z-50 top-[150px]">*/}
-            {/*    <video autoPlay loop muted>*/}
-            {/*        <source src="assets/Rounded%20Purple%20lines%20Abstract%20Gradient%20Background%20Animation_1.gif"*/}
-            {/*                type="video/mp4"/>*/}
-            {/*    </video>*/}
-            {/*</div>*/}
-            {/*<img src="assets/Rounded%20Purple%20lines%20Abstract%20Gradient%20Background%20Animation_1.gif" className="w-full h-full"/>*/}
-            
+            {/*<Header/>*/}
+
+            <Home />
         
-        
+            <BackgroundVideo />
+
         </>
     );
 }
