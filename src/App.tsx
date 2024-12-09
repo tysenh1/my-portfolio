4bd30adec8882271c0acc5908be5ffb19205b9ff
Tysen Heck <theck@academic.rrc.ca>
2024-12-09 01:21:04 -0600

    Tried a lot of random css animations and settled on the video background again lmao (this is not a good way to do this please find another way.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import "./index.css";
 import Header from "./components/Header.tsx";
 
+
 function App() {
     
     return (
         <>
-            {/*<Header />*/}
-            <img src="assets/arrow-left.svg"/>
-
-            <video autoPlay loop muted>
-                <source src="/src/assets/chill-blue-blob-slow-LONG.mp4" type="video/mp4" />
-            </video>
-
+            <Header/>
+            {/*<div className="z-50 top-[150px]">*/}
+            {/*    <video autoPlay loop muted>*/}
+            {/*        <source src="assets/Rounded%20Purple%20lines%20Abstract%20Gradient%20Background%20Animation_1.gif"*/}
+            {/*                type="video/mp4"/>*/}
+            {/*    </video>*/}
+            {/*</div>*/}
+            {/*<img src="assets/Rounded%20Purple%20lines%20Abstract%20Gradient%20Background%20Animation_1.gif" className="w-full h-full"/>*/}
+            
+        
+        
         </>
     );
 }
