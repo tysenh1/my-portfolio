668319917eb8eac3f70518cb38d2f168295a09cb
Tysen Heck <theck@academic.rrc.ca>
2024-12-12 15:12:36 -0600

    Made a lower fps version of jellyfish jam.

--- src/BackgroundVideo.tsx
+++ src/BackgroundVideo.tsx
@@ -2,7 +2,7 @@
 function BackgroundVideo() {
     return (
         <div className={" absolute -z-50 top-0"}>
-            <video autoPlay loop muted
+            <video autoPlay loop muted preload={"auto"}
                    className="object-cover mask-repeat-no-repeat mask-size-cover mask-image-radial-at-c from-black to-transparent to-50% blur-2xl h-[954px]">
                 <source src="https://raw.githubusercontent.com/tysenh1/my-portfolio/refs/heads/main/src/assets/chill-blue-blob-slow-SHORT.mp4" type="video/mp4"/>
             </video>
