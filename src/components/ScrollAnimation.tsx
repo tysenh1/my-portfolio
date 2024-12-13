d631265d860250c2e0a10436fa6656452360fb30
Tysen Heck <theck@academic.rrc.ca>
2024-12-12 22:04:51 -0600

    Added placeholder images for the canvas option for playing a video based on the scroll stuff.

--- src/components/ScrollAnimation.tsx
+++ src/components/ScrollAnimation.tsx
@@ -1,26 +1,18 @@
 import {motion, useScroll, useTransform, useMotionValueEvent} from "framer-motion";
-import {useEffect, useRef} from "react";
+import {useRef, useEffect, useState} from "react";
 
 
 function ScrollAnimation() {
     
     const scrollRef = useRef(null)
     const videoRef = useRef<HTMLVideoElement>(null);
+    const totalFrames = 112;
     
     // const { scrollYProgress } = useScroll({ container: scrollRef})
     const { scrollYProgress } = useScroll({
         target: scrollRef,
         offset: ['start start', 'end end']
     })
-    
-    const playbackTime = useTransform(scrollYProgress, [0, 1], [0, 0.5]);
-    
-    useMotionValueEvent(playbackTime, "change", (latest) => {
-        if (videoRef.current) {
-            const duration = videoRef.current.duration || 1;
-            videoRef.current.currentTime = latest * duration;
-        }
-    });
     
     const rotateYPurple = useTransform(
         scrollYProgress,
@@ -34,11 +26,19 @@ function ScrollAnimation() {
         ['180deg', '360deg']
     )
     
+
+    
     return (
+        // <div
+        //     className={"relative h-[6000px]"}
+        // >
+        //     <canvas ref={canvasRef} className="sticky top-0 right-0"></canvas>
+        // </div>
+    
             <div className={"h-[7000px] w-full"} ref={scrollRef}>
                 <div className={"w-full px-auto h-auto perspective sticky top-0 "}
                      style={{transformStyle: 'preserve-3d'}}>
-                    
+
                     <motion.img
                         src={"https://github.com/tysenh1/my-portfolio/blob/main/src/assets/purple-square.png?raw=true"}
                         style={{
@@ -53,39 +53,17 @@ function ScrollAnimation() {
                             transformOrigin: 'center center',
                             backfaceVisibility: 'hidden'
                         }} className={"top-24 left-24 absolute"}/>
-                    
+
                     <video muted preload={"auto"}
-                           className="h-[300px] absolute right-0 top-0"
-                            ref={videoRef}>
+                           className="h-[300px] absolute right-0 top-0 hidden"
+                           ref={videoRef}>
                         <source
                             src="https://raw.githubusercontent.com/tysenh1/my-portfolio/refs/heads/main/src/assets/Jellyfish-Jam_1.mp4"
                             type="video/mp4"/>
                     </video>
-                
+
                 </div>
             </div>
-        
-        // <div
-        //     ref={scrollRef}
-        //     style={{
-        //         height: "100vh", // Height of the parent element
-        //         width: "100%",
-        //         display: "flex",
-        //         justifyContent: "center",
-        //         alignItems: "center",
-        //         backgroundColor: "#f0f0f0",
-        //     }}
-        // >
-        //     <motion.img
-        //         src="your-image.jpg"
-        //         alt="Rotating"
-        //         style={{
-        //             width: "200px",
-        //             height: "200px",
-        //             rotateY: rotateYPurple, // Rotate based on scroll progress
-        //         }}
-        //     />
-        // </div>
 
 )
 }
