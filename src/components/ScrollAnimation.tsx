f3c94d441ca18bb3c3758b029df0301165cff292
Tysen Heck <theck@academic.rrc.ca>
2024-12-14 20:49:12 -0600

    Added fade in animations and made an extra slide for figma n stuff or whatever else I want to put there.

--- src/components/ScrollAnimation.tsx
+++ src/components/ScrollAnimation.tsx
@@ -16,18 +16,69 @@ function ScrollAnimation() {
         offset: ['start start', 'end end']
     })
     
-    const imageOneOpacity = useTransform(scrollYProgress, [0, 0.05], ['0%', '100%'])
-    const imageOneX = useTransform(scrollYProgress, [0, 0.05], [-100, 0])
     
-    const imageTwoOpacity = useTransform(scrollYProgress, [0.475, 0.525], ['0%', '100%'])
-    const imageTwoX = useTransform(scrollYProgress, [0.475, 0.525], [-100, 0])
+    const [imageOneOpacity, imageTwoOpacity, imageThreeOpacity] = [
+        useTransform(scrollYProgress, [0, 0.06, 0.3, 0.36], [0, 1, 1 ,0]),
+        useTransform(scrollYProgress, [0.3, 0.36, 0.63, 0.7], [0, 1, 1 ,0]),
+        useTransform(scrollYProgress, [0.63, 0.7], [0, 1])
+    ]
+    
+    const [imageOneX, imageTwoX, imageThreeX] = [
+        useTransform(scrollYProgress, [0, 0.06, 0.3, 0.36], [-100, 0, 0, 100]),
+        useTransform(scrollYProgress, [0.3, 0.36, 0.63, 0.7], [-100, 0, 0, 100]),
+        useTransform(scrollYProgress, [0.63, 0.7], [-100, 0])
+    ]
+    
+    const [imageOneOpacitySpring, imageTwoOpacitySpring, imageThreeOpacitySpring] = [
+        useSpring(imageOneOpacity, { stiffness: 150, damping: 35 }),
+        useSpring(imageTwoOpacity, { stiffness: 150, damping: 35 }),
+        useSpring(imageThreeOpacity, { stiffness: 150, damping: 35 })
+    ]
+    
+    const [imageOneXSpring, imageTwoXSpring, imageThreeXSpring] = [
+        useSpring(imageOneX, { stiffness: 100, damping: 35 }),
+        useSpring(imageTwoX, { stiffness: 100, damping: 35 }),
+        useSpring(imageThreeX, { stiffness: 100, damping: 35 })
+    ]
     
     
-    const imageOneOpacitySpring = useSpring(imageOneOpacity, { stiffness: 100, damping: 30 })
-    const imageTwoOpacitySpring = useSpring(imageTwoOpacity, { stiffness: 100, damping: 30 })
     
-    const imageOneXSpring = useSpring(imageOneX, { stiffness: 150, damping: 35 })
-    const imageTwoXSpring = useSpring(imageTwoX, { stiffness: 150, damping: 35 })
+    const [textOneOpacity, textTwoOpacity, textThreeOpacity] = [
+        useTransform(scrollYProgress, [0, 0.06, 0.3, 0.36], [0, 1, 1 ,0]),
+        useTransform(scrollYProgress, [0.3, 0.36, 0.63, 0.7], [0, 1, 1 ,0]),
+        useTransform(scrollYProgress, [0.63, 0.7], [0, 1])
+    ]
+    
+    const [textOneY, textTwoY, textThreeY] = [
+        useTransform(scrollYProgress, [0, 0.06, 0.3, 0.36], [100, 0, 0, -100]),
+        useTransform(scrollYProgress, [0.3, 0.36, 0.63, 0.7], [100, 0, 0, -100]),
+        useTransform(scrollYProgress, [0.63, 0.7], [100, 0])
+    ]
+    
+    const [textOneOpacitySpring, textTwoOpacitySpring, textThreeOpacitySpring] = [
+        useSpring(textOneOpacity, { stiffness: 150, damping: 35 }),
+        useSpring(textTwoOpacity, { stiffness: 150, damping: 35 }),
+        useSpring(textThreeOpacity, { stiffness: 150, damping: 35 })
+    ]
+    
+    const [textOneYSpring, textTwoYSpring, textThreeYSpring] = [
+        useSpring(textOneY, { stiffness: 100, damping: 35 }),
+        useSpring(textTwoY, { stiffness: 100, damping: 35 }),
+        useSpring(textThreeY, { stiffness: 100, damping: 35 })
+    ]
+    
+    // const imageOneOpacity = useTransform(scrollYProgress, [0, 0.05], ['0%', '100%'])
+    // const imageOneX = useTransform(scrollYProgress, [0, 0.05], [-100, 0])
+    //
+    // const imageTwoOpacity = useTransform(scrollYProgress, [0.475, 0.525], ['0%', '100%'])
+    // const imageTwoX = useTransform(scrollYProgress, [0.475, 0.525], [-100, 0])
+    //
+    //
+    // const imageOneOpacitySpring = useSpring(imageOneOpacity, { stiffness: 100, damping: 30 })
+    // const imageTwoOpacitySpring = useSpring(imageTwoOpacity, { stiffness: 100, damping: 30 })
+    //
+    // const imageOneXSpring = useSpring(imageOneX, { stiffness: 150, damping: 35 })
+    // const imageTwoXSpring = useSpring(imageTwoX, { stiffness: 150, damping: 35 })
     
     // const rotateYImage1 = useTransform(
     //     scrollYProgress,
@@ -53,10 +104,64 @@ function ScrollAnimation() {
             <div className={"w-full px-auto h-auto perspective sticky top-0 "}
                  style={{transformStyle: 'preserve-3d'}}
             >
-                
-                <div>
-                    <
+                <div className={"w-full h-[100vh] flex"}>
+                    <div
+                        className={"w-[50vw] flex justify-center items-center h-[100vh]"}
+                    >
+                        {/*<motion.canvas className={"w-[300px] h-[200px]"}></motion.canvas>*/}
+                        <motion.div
+                            className={"w-[600px] h-[500px] bg-blue-700 absolute"}
+                            style={{
+                                opacity: imageOneOpacitySpring,
+                                x: imageOneXSpring
+                            }}
+                        ></motion.div>
+                        
+                        <motion.div
+                            className={"w-[600px] h-[500px] bg-amber-400 absolute"}
+                            style={{
+                                opacity: imageTwoOpacitySpring,
+                                x: imageTwoXSpring
+                            }}
+                        ></motion.div>
+                        
+                        <motion.div
+                            className={"w-[600px] h-[500px] bg-lime-500 absolute"}
+                            style={{
+                                opacity: imageThreeOpacitySpring,
+                                x: imageThreeXSpring
+                            }}
+                        ></motion.div>
+                    </div>
+                    <div
+                        className={"w-[50vw] flex justify-center items-center h-[100vh]"}
+                    >
+                        <motion.p
+                            className={"text-white absolute"}
+                            style={{
+                                opacity: textOneOpacitySpring,
+                                y: textOneYSpring
+                            }}
+                        >BANANA</motion.p>
+                        
+                        <motion.p
+                            className={"text-white absolute"}
+                            style={{
+                                opacity: textTwoOpacitySpring,
+                                y: textTwoYSpring
+                            }}
+                        >England! England! England!</motion.p>
+                        
+                        <motion.p
+                            className={"text-white absolute"}
+                            style={{
+                                opacity: textThreeOpacitySpring,
+                                y: textThreeYSpring
+                            }}
+                        >No no no no, Kevin, let me do it, let me do it, spita.</motion.p>
+                    </div>
                 </div>
+                
                 
                 {/*<motion.img*/}
                 {/*    src={"https://github.com/tysenh1/my-portfolio/blob/main/src/assets/purple-square.png?raw=true"}*/}
