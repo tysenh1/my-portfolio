64dda6533c869527d5b3db0bcfa390a55bd55249
Tysen Heck <theck@academic.rrc.ca>
2024-12-17 17:58:11 -0600

    Added some more animations to the page and tried to add an animation presence thing for all the content at the bottom, that is work in progress tho

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 import Resume from "./Resume/Resume"
 import CoverLetter from "./CoverLetter"
 import Interview from "./Interview/Interview"
@@ -7,94 +7,112 @@ import { SlideLeftButton, SlideRightButton } from "./SlideButton"
 
 // @ts-ignore
 function AboutMe() {
-
-  const [slideCounter, setSlideCounter] = useState(0)
-  const [slideState, setSlideState] = useState('right')
-  const [slideInitial, setSlideInitial] = useState({ opacity: 0, x: 100 })
-  const [slideExit, setSlideExit] = useState({ opacity: 0, x: -100 })
-
-
-  function handleLeftClick(slideCounter: number) {
-    slideCounter == 0 ? setSlideCounter(2) : setSlideCounter(slideCounter-1)
-  }
-
-  function handleRightClick(slideCounter: number) {
-    slideCounter == 2 ? setSlideCounter(0) : setSlideCounter(slideCounter+1)
-  }
-
-  useEffect(() => {
-    if (slideState == 'right') {
-      setSlideInitial({ opacity: 0, x: 100 })
-      setSlideExit({ opacity: 0, x: -100})
-    } else {
-      setSlideInitial({ opacity: 0, x: -100 })
-      setSlideExit({ opacity: 0, x: 100 })
+    
+    const [slideCounter, setSlideCounter] = useState(0)
+    const [slideState, setSlideState] = useState('right')
+    const [slideInitial, setSlideInitial] = useState({ opacity: 0, x: 100 })
+    const [slideExit, setSlideExit] = useState({ opacity: 0, x: -100 })
+    const aboutRef = useRef<HTMLDivElement>(null)
+    const [parentHeight, setParentHeight] = useState(0);
+    
+    
+    function handleLeftClick(slideCounter: number) {
+        slideCounter == 0 ? setSlideCounter(2) : setSlideCounter(slideCounter-1)
     }
     
-  }, [slideState])
-
-  // TODO This animation is scuffed but it works and that is all that matter right now
-  // useEffect(() => {window.scrollTo({ top: 700, behavior: 'smooth' })}, [])
-
-  return (
-    <>
-      <div className="relative w-full h-auto mt-12 mb-12" id="aboutme">
-        <div className="relative w-full flex justify-center mt-4">
-          <div onMouseEnter={() => setSlideState('left')} className="mx-auto mb-12">
-            <SlideLeftButton handleClick={handleLeftClick} slideCounter={slideCounter} />
-          </div>
-          <div onMouseEnter={() => setSlideState('right')} className="mx-auto mb-12">
-            <SlideRightButton handleClick={handleRightClick} slideCounter={slideCounter} />
-          </div>
-          
-        </div>
-        <AnimatePresence>
-          <motion.div
-
-          // TODO Come back to this and try out custom animations to add a delay between the first one and the second starting
-          // And also just fade out the first component maybe when it leaves.
-          // This will probably be done by adding a delay before the component is rendered in the dom, rather than adding a delay on the animations
-
-          // TODO Come back to this after the comms stuff is due too because we all know you aint gonna figure this out until after that.
-            key={slideCounter}
-            // initial={{ opacity: 0, x: 100 }}
-            initial={slideInitial}
-            animate={{ opacity: 1, x: 0 }}
-            // exit={{ opacity: 0, x: -100 }}
-            exit={slideExit}
-
-            transition={{ duration: 0.5 }}
-            style={{ position: 'absolute' }}
-            className="h-auto mt-4 w-full"
-          >
-            {
-              slideCounter === 0 ? <CoverLetter handleLeftClick={handleLeftClick} handleRightClick={handleRightClick} slideCounter={slideCounter}/>
-              : slideCounter === 1 ? <Resume handleLeftClick={handleLeftClick} handleRightClick={handleRightClick} slideCounter={slideCounter}/>
-              : <Interview handleLeftClick={handleLeftClick} handleRightClick={handleRightClick} slideCounter={slideCounter}/>
-            }
-          </motion.div>
-        </AnimatePresence>
+    function handleRightClick(slideCounter: number) {
+        slideCounter == 2 ? setSlideCounter(0) : setSlideCounter(slideCounter+1)
+    }
+    
+    useEffect(() => {
+        if (slideState == 'right') {
+            setSlideInitial({ opacity: 0, x: 100 })
+            setSlideExit({ opacity: 0, x: -100})
+        } else {
+            setSlideInitial({ opacity: 0, x: -100 })
+            setSlideExit({ opacity: 0, x: 100 })
+        }
         
-        {/* Button container positioned below the animated content */}
-        <div className="relative w-full flex justify-center mt-4">
-{/*
+    }, [slideState])
+    
+    useEffect(() => {
+        if (aboutRef.current) {
+            setParentHeight(aboutRef.current.offsetHeight + 106);
+            console.log(aboutRef.current.offsetHeight)
+        }
+    }, [slideCounter]);
+    
+    // TODO This animation is scuffed as shit but it works and that is all that matter right now
+    // useEffect(() => {window.scrollTo({ top: 700, behavior: 'smooth' })}, [])
+    
+    return (
+        <>
+            <div className={`relative w-full mt-12 mb-12`} id="aboutme" style={{ height: `${parentHeight || 600}px` }}>
+                <div className="relative w-full flex justify-center mt-4">
+                    <div onMouseEnter={() => setSlideState('left')} className="mx-auto mb-12">
+                        <SlideLeftButton handleClick={handleLeftClick} slideCounter={slideCounter} />
+                    </div>
+                    <div onMouseEnter={() => setSlideState('right')} className="mx-auto mb-12">
+                        <SlideRightButton handleClick={handleRightClick} slideCounter={slideCounter} />
+                    </div>
+                
+                </div>
+                <AnimatePresence>
+                    <motion.div
+                        
+                        // TODO Come back to this and try out custom animations to add a delay between the first one and the second starting
+                        // And also just fade out the first component maybe when it leaves.
+                        // This will probably be done by adding a delay before the component is rendered in the dom, rather than adding a delay on the animations
+                        
+                        // TODO Come back to this after the comms stuff is due too because we all know you aint gonna figure this out until after that.
+                        key={slideCounter}
+                        initial={slideInitial}
+                        animate={{ opacity: 1, x: 0 }}
+                        exit={slideExit}
+                        // onAnimationComplete={() => {
+                        //     setTopHeight('0px')
+                        // }}
+                        // onAnimationStart={() => {
+                        //     slideCounter === 0 ? setTopHeight('668px')
+                        //     : slideCounter === 1 ? setTopHeight('1502px')
+                        //     : setTopHeight('1087px')
+                        // }}
+                        
+                        transition={{ duration: 0.5 }}
+                        style={{
+                            position: 'absolute',
+                        }}
+                        className="w-full"
+                        ref={aboutRef}
+                    >
+                        {
+                            slideCounter === 0 ? <CoverLetter />
+                                : slideCounter === 1 ? <Resume />
+                                    : <Interview />
+                        }
+                    </motion.div>
+                </AnimatePresence>
+                
+                {/* Button container positioned below the animated content */}
+                <div className="relative w-full flex justify-center mt-4">
+                    {/*
 // ==================================================================================== //
 // ==================== Buttons for navigating the about me slides ==================== //
 // ==================================================================================== //
 */}
-
-          {/* <a onClick={() => handleLeftClick(slideCounter)} className="mx-auto z-50 text-white">
+                    
+                    {/* <a onClick={() => handleLeftClick(slideCounter)} className="mx-auto z-50 text-white">
             LEFT
           </a> */}
-          {/* <a onClick={() => handleRightClick(slideCounter)} className="mx-auto z-50 text-white">
+                    {/* <a onClick={() => handleRightClick(slideCounter)} className="mx-auto z-50 text-white">
             RIGHT
           </a> */}
-        </div>
-      </div>
-
-      
-    </>
-  )
+                </div>
+            </div>
+        
+        
+        </>
+    )
 }
 
 export default AboutMe
--- src/components/NavHeaderButton.tsx
+++ src/components/NavHeaderButton.tsx
@@ -13,29 +13,33 @@ function NavHeaderButton({navText, buttonVariants, buttonIndicatorVariants, acti
     
     
     return (
-        <motion.div
-            onClick={() => {
-                handleNavClick(buttonId)
-                setPageContent(navText)
-            }}
-            className={"mt-4 mb-0 flex flex-col mx-auto cursor-pointer"}
-            variants={buttonVariants}
-            initial="initial"
-            animate={activeButton === `${buttonId}` ? 'active' : 'inactive'}
-            transition={{duration: 0.2, ease: 'easeOut'}}
-            style={{transformOrigin: 'bottom'}}
-            // whileHover={activeButton === `${buttonId}` ? '' : 'hover'}
-        >
-            <p className={`text-2xl font-light text-white mx-auto mb-1`}>{navText}</p>
+
             <motion.div
-                className={`w-20 h-2 bg-white mx-auto rounded-tl-full rounded-tr-full clip-class`}
-                variants={buttonIndicatorVariants}
+                
+                className={"flex flex-col mb-0 mt-auto w-[200px] mx-auto"}
+                variants={buttonVariants}
                 initial="initial"
                 animate={activeButton === `${buttonId}` ? 'active' : 'inactive'}
-                transition={{duration: 0.3}}
-            ></motion.div>
+                transition={{duration: 0.4, ease: 'easeOut'}}
+                style={{transformOrigin: 'bottom'}}
+                // whileHover={activeButton === `${buttonId}` ? '' : 'hover'}
+            >
+                <p
+                    className={`text-4xl font-light text-white mx-auto mb-4 cursor-pointer`}
+                    onClick={() => {
+                        handleNavClick(buttonId)
+                        setPageContent(navText)
+                    }}
+                >{navText}</p>
+                <motion.div
+                    className={`w-20 h-3 bg-white mx-auto rounded-tl-full rounded-tr-full clip-class`}
+                    variants={buttonIndicatorVariants}
+                    initial="initial"
+                    animate={activeButton === `${buttonId}` ? 'active' : 'inactive'}
+                    transition={{duration: 0.3}}
+                ></motion.div>
+            </motion.div>
 
-        </motion.div>
     )
 }
 
