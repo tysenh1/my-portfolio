d3441c01746f58c6b8e71801160c52e7e75201ab
Tysen Heck <theck@academic.rrc.ca>
2024-12-17 20:37:23 -0600

    Kinda fixed the animations and page heights. It is very scuffed though.

--- src/components/Other/Other.tsx
+++ src/components/Other/Other.tsx
@@ -1,11 +1,37 @@
 import PageConstruction from "../PageConstruction.tsx";
+import {useEffect, useRef} from "react";
+import {motion} from "framer-motion";
 
 
-function Other() {
+function Other({setPageHeight}) {
+    const otherRef = useRef<HTMLDivElement>(null)
     
+    const variants = {
+        initial: { opacity: 0, y: 50 },
+        animate: { opacity: 100, y: 0 }
+    }
+    
+    useEffect(() => {
+        if (otherRef.current) {
+            setPageHeight(otherRef.current.offsetHeight + 96)
+        }
+    }, [otherRef]);
     
     return (
-        <PageConstruction />
+        <motion.div
+            className={"relative w-full h-auto my-12"}
+            variants={variants}
+            animate={'animate'}
+            // initial={'initial'}
+            transition={{
+                duration: 0.5,
+                ease: 'easeOut'
+            }}
+            ref={otherRef}
+        >
+            <PageConstruction/>
+        
+        </motion.div>
     )
 }
 
