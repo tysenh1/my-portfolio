862f0049bb30b754f8f498be1ce18039c1d9d983
Tysen Heck <theck@academic.rrc.ca>
2024-12-18 01:25:41 -0600

    Started working on the tech stack portion of the coding tab.

--- src/components/Coding/CodingHeader.tsx
+++ src/components/Coding/CodingHeader.tsx
@@ -1,11 +1,14 @@
-function CodingHeader({headerText}: {headerText: string}) {
+
+function ResumeHeader({headerText}: {
+    headerText: string
+}) {
     return (
         <>
-            <div className="h-[1px] bg-white mx-6 my-6"></div>
             <h3 className="text-white text-3xl font-light font-roboto mb-8">{headerText}</h3>
             <div className="h-[1px] bg-white mx-6 my-6"></div>
         </>
     )
 }
 
-export default CodingHeader
+
+export default ResumeHeader
--- src/components/Coding/TechStack.tsx
+++ src/components/Coding/TechStack.tsx
@@ -1,174 +1,40 @@
-import React, {MutableRefObject, RefObject} from "react";
-import {motion} from "motion/react";
-import TechStackPopup from "./TechStackPopup.tsx";
 
-function TechStack({
-    animationStates,
-    refs,
-    techPopupParentRef,
-    isAnimated,
-    setIsAnimated,
-    popupWidth,
-    setPopupWidth,
-    techIndex,
-    setTechIndex,
-    stackArray,
-    activeIndex,
-    setActiveIndex
-                   }: {
-    animationStates: object,
-    refs: MutableRefObject<(HTMLDivElement | null)[]>,
-    techPopupParentRef: RefObject<HTMLDivElement>,
-    isAnimated: boolean,
-    setIsAnimated: React.Dispatch<React.SetStateAction<boolean>>,
-    popupWidth: number,
-    setPopupWidth: React.Dispatch<React.SetStateAction<number>>,
-    techIndex: number,
-    setTechIndex: React.Dispatch<React.SetStateAction<number>>,
-    stackArray: string[],
-    activeIndex: number | null,
-    setActiveIndex: React.Dispatch<React.SetStateAction<number | null>>
-}) {
-    // const refs = useRef<(HTMLDivElement | null)[]>([])
-    // const [activeIndex, setActiveIndex] = useState<number | null>(null)
-    // const techPopupParentRef = useRef<HTMLDivElement>(null)
-    // const [isAnimated, setIsAnimated] = useState<boolean>(false)
-    // const [popupWidth, setPopupWidth] = useState(0)
-    // const [techIndex, setTechIndex] = useState(0)
+function TechStack() {
+    const stackArray = [
+        'Python',
+        'JavaScript',
+        'HTML',
+        'CSS',
+        'Node.js/TypeScript',
+        'GoLang',
+        'Django',
+        'React.js',
+        'TailwindCSS',
+        'Framer Motion',
+        'Figma',
+        'NestJS',
+        'TypeORM',
+        'Slonik',
+        'PostgreSQL',
+        'MariaDB',
+        'MongoDB',
+        'VSCodium',
+        'WebStorm',
+        'Git/GitHub'
+    ]
     
-    // const animationStates = {
-    //     initial: {
-    //       display: 'none',
-    //       height: '0px',
-    //       width: '0px'
-    //     },
-    //     heightInitial: {
-    //         height: 'auto'
-    //     },
-    //     heightAnimate: {
-    //         height: ['72px', '400px', '400px'],
-    //         marginTop: ['12px', '24px', '24px'],
-    //         transition: {
-    //             duration: 0.75,
-    //             times: [0, 0.4, 1]
-    //         }
-    //     },
-    //     heightNotAnimated: {
-    //         height: ['400px', '400px', '72px'],
-    //         marginTop: ['24px', '24px', '12px'],
-    //         transition: {
-    //             duration: 0.75,
-    //             times: [0, 0.6, 1]
-    //         }
-    //
-    //     },
-    //     widthAnimate: {
-    //         display: 'flex',
-    //         width: ['0%', '0%', `${popupWidth}px`],
-    //         marginTop: ['24px', '24px', '12px'],
-    //         height: ['0px', '400px', '400px'],
-    //         transition: {
-    //             duration: 0.75,
-    //             times: [0, 0.4, 1]
-    //         }
-    //     },
-    //     notAnimated: {
-    //         display: 'none',
-    //         height: ['400px', '400px', '0px'],
-    //         marginTop: ['24px', '24px', '12px'],
-    //         width: ['100%', '0%', '0%'],
-    //
-    //         transition: {
-    //             duration: 0.75,
-    //             times: [0, 0.6, 1]
-    //         }
-    //     }
-    // }
-    
-    // const stackArray = [
-    //     'Python',
-    //     'JavaScript',
-    //     'HTML',
-    //     'CSS',
-    //     'Node.js/TypeScript',
-    //     'GoLang',
-    //     'SQL',
-    //     'Django',
-    //     'React.js',
-    //     'TailwindCSS',
-    //     'Framer Motion',
-    //     'Figma',
-    //     'NestJS',
-    //     'TypeORM',
-    //     'Slonik',
-    //     'PostgreSQL',
-    //     'MariaDB',
-    //     'MongoDB',
-    //     'VSCode',
-    //     'WebStorm',
-    //     'Git/GitHub'
-    // ]
-    
-    const handleClick = (index: number) => {
-        setTechIndex(index)
-        if (refs.current[index]) {
-            setActiveIndex(activeIndex === index ? null : index)
-            
-        }
-        if (techPopupParentRef.current) {
-            setPopupWidth(techPopupParentRef.current.offsetWidth)
-            // console.log(techPopupParentRef.current.offsetWidth)
-        }
+    const tech = stackArray.map((tech) => {
+        return (
+            <div className={"border-2 border-gray-300 w-auto h-auto p-1 justify-center items-center inline-flex rounded-xl mx-1"}>
+                <p className={"text-white text-sm"}>{tech}</p>
+            </div>
         
-    }
-    
-    
+        )
+    })
     
     return (
-        <div className={"relative pt-3"}>
-            <div className={"relative w-full z-10"} ref={techPopupParentRef}>
-                <motion.div
-                    className={"right-0 absolute border-gray-300 border-2 backdrop-blur-lg rounded-3xl"}
-                    
-                    variants={animationStates}
-                    initial={"initial"}
-                    animate={isAnimated ? 'widthAnimate' : 'notAnimated'}
-                    // onClick={() => setIsAnimated(isAnimated ? false : true)}
-                    ref={techPopupParentRef}
-                >
-                    <TechStackPopup index={techIndex} width={popupWidth} setIsAnimated={setIsAnimated} isAnimated={isAnimated}/>
-                    {/*<img*/}
-                    {/*    src={"/my-portfolio/x.svg"}*/}
-                    {/*    className={"w-10 h-10 absolute right-4 top-4 cursor-pointer"}*/}
-                    {/*    onClick={() => setIsAnimated(false)}*/}
-                    {/*/>*/}
-                </motion.div>
-            </div>
-            
-            <motion.div
-                
-                variants={animationStates}
-                initial={false}
-                animate={isAnimated ? 'heightAnimate' : 'heightNotAnimated'}
-                className={""}
-            >
-                {stackArray.map((tech, index) => {
-                    return (
-                        <div
-                            className={"border-2 border-gray-300 px-1 py-0.5 justify-center items-center inline-flex rounded-lg mx-1 cursor-pointer top-0 mb-2"}
-                            onClick={() => {
-                                setIsAnimated(isAnimated ? false : true)
-                                handleClick(index)
-                            }}
-                            key={index}
-                        >
-                            <p className={"text-white text-sm"}>{tech}</p>
-                        </div>
-                    )
-                })}
-            </motion.div>
-        
-        
+        <div>
+            {tech}
         </div>
     )
 }
